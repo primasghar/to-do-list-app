c440c6ad72190367ab8b83ffda3d0d3e3e7e5c44
Prim Asghar <mirpasghar@gmail.com>
2022-03-07 23:52:33 +0200

    Added form component

--- src/ToDo/components/AddForm/AddForm.js
+++ src/ToDo/components/AddForm/AddForm.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import classes from "./AddForm.module.css";
+import Button from "../../../Shared/Button/Button";
 
-function AddForm() {
-  return <div>AddForm</div>;
-}
+const AddForm = (props) => {
+  const [enteredTask, setEnteredTask] = useState("");
+
+  const onInputChange = (event) => {
+    setEnteredTask(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const taskData = {
+      para: enteredTask,
+    };
+    props.onSaveTask(taskData);
+    setEnteredTask("");
+  };
+
+  return (
+    <form className={classes.container} onSubmit={submitHandler}>
+      <label htmlFor="task">Enter task</label>
+      <input
+        type="text"
+        id="task"
+        className={classes.task}
+        onChange={onInputChange}
+        value={enteredTask}
+      />
+      <Button type="submit">Add</Button>
+    </form>
+  );
+};
 
 export default AddForm;
