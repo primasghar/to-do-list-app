2078a4bd59a6ff9a622a3438bcb05c907dfbbafb
Prim Asghar <mirpasghar@gmail.com>
2022-03-08 00:05:42 +0200

    Changed the headings name in ToDo component

--- src/ToDo/ToDo.js
+++ src/ToDo/ToDo.js
@@ -1,17 +1,32 @@
-import AddToDo from "./components/AddForm/AddForm";
+import React, { useState } from "react";
+
+import AddForm from "./components/AddForm/AddForm";
 import ToDoList from "./components/ToDoList/ToDoList";
 
 import classes from "./ToDo.module.css";
 
+const DUMMY_TASKS = [
+  { para: "Practice React" },
+  { para: "Learn Finnish" },
+  { para: "Improve drawing skills" },
+];
+
 function ToDo() {
+  const [tasks, setTasks] = useState(DUMMY_TASKS);
+
+  const saveTaskHandler = (enteredTaskData) => {
+    setTasks((prevTasks) => {
+      return [enteredTaskData, ...prevTasks];
+    });
+  };
   return (
-    <main>
-      <h1 className={classes.heading}>ToDo app</h1>
-      <AddToDo />
-      <h1>To do</h1>
-      <ToDoList />
-      <h1>Done</h1>
-      <ToDoList />
+    <main className={classes.wrapper}>
+      <h1 className={classes.heading}>To-Do-list</h1>
+      <AddForm onSaveTask={saveTaskHandler} />
+      <h2>Open Tasks </h2>
+      <ToDoList tasks={tasks} />
+      <h2>Done Tasks</h2>
+      <ToDoList tasks={[]} />
     </main>
   );
 }
