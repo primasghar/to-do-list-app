19e15ce17a0b7337cd32d2a1a3e799461603b2aa
Prim Asghar <mirpasghar@gmail.com>
2022-03-07 23:50:24 +0200

    Added ToDoList component

--- src/ToDo/components/ToDoList/ToDoList.js
+++ src/ToDo/components/ToDoList/ToDoList.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ToDoItem from "../ToDoItem/ToDoItem";
+import classes from "./ToDoList.module.css";
 
-function ToDoList() {
+const ToDoList = (props) => {
   return (
-    <div>
-      <ToDoItem />
-      <ToDoItem />
-      <ToDoItem />
-    </div>
+    <ul className={classes.list}>
+      {!props.tasks.length && "no tasks"}
+      {props.tasks.map((task, index) => (
+        <ToDoItem para={task.para} key={index} />
+      ))}
+    </ul>
   );
-}
+};
 
 export default ToDoList;
