5f04b91e753954410703481f9f88d2ddbee010c7
Prim Asghar <mirpasghar@gmail.com>
2022-03-09 20:43:19 +0200

    Restored to simple <p> tag and added onClick prop

--- src/ToDo/components/ToDoItem/ToDoItem.js
+++ src/ToDo/components/ToDoItem/ToDoItem.js
@@ -1,13 +1,10 @@
 import React from "react";
 import classes from "./ToDoItem.module.css";
 
-const ToDoItem = ({ para }) => {
+const ToDoItem = ({ text, handleTask }) => {
   return (
-    <li className={classes.item}>
-      <form>
-        <input className={classes.checkbox} id="checker" type="checkbox" />
-        <label htmlFor="checker">{para}</label>
-      </form>
+    <li className={classes.item} onClick={handleTask}>
+      <p>{text}</p>
     </li>
   );
 };
