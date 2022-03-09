be788d31db15f123abf436a719a0b28f7d52eeef
Prim Asghar <mirpasghar@gmail.com>
2022-03-09 21:00:41 +0200

    Added useStates, Data lifting, donetask, opentask handler & lists updating fn

--- src/ToDo/ToDo.js
+++ src/ToDo/ToDo.js
@@ -5,28 +5,59 @@ import ToDoList from "./components/ToDoList/ToDoList";
 
 import classes from "./ToDo.module.css";
 
-const DUMMY_TASKS = [
-  { para: "Practice React" },
-  { para: "Learn Finnish" },
-  { para: "Improve drawing skills" },
-];
-
 function ToDo() {
-  const [tasks, setTasks] = useState(DUMMY_TASKS);
+  const [openTasks, setOpenTasks] = useState([]);
+  const [doneTasks, setDoneTasks] = useState([]);
 
-  const saveTaskHandler = (enteredTaskData) => {
-    setTasks((prevTasks) => {
-      return [enteredTaskData, ...prevTasks];
+  const addTaskToOpen = (openTask) => {
+    setOpenTasks((prevTasks) => {
+      return [openTask, ...prevTasks];
     });
   };
+
+  const addTaskToDone = (doneTask) => {
+    setDoneTasks((prevTasks) => {
+      return [doneTask, ...prevTasks];
+    });
+  };
+
+  const handleOpenTask = (openTasks, doneTask) => {
+    const doneTaskIndex = openTasks.findIndex((task) => task === doneTask);
+
+    const filteredOpenTasks = openTasks.filter(
+      (task, index) => index !== doneTaskIndex
+    );
+
+    // reset the Open Tasks list after removing the done tasks
+    setOpenTasks(filteredOpenTasks);
+
+    //Adding the task to the Done Task list
+    addTaskToDone(doneTask);
+  };
+
+  const handleDoneTask = (doneTasks, taskToOpen) => {
+    const taskIndex = doneTasks.findIndex((task) => task === taskToOpen);
+
+    const filteredDoneTasks = doneTasks.filter(
+      (task, index) => index !== taskIndex
+    );
+
+    setDoneTasks(filteredDoneTasks);
+
+    // reset the open tasks after removing the done tasks
+    addTaskToOpen(taskToOpen);
+  };
+
+  const saveTaskHandler = (enteredTaskData) => addTaskToOpen(enteredTaskData);
+
   return (
     <main className={classes.wrapper}>
       <h1 className={classes.heading}>To-Do-list</h1>
       <AddForm onSaveTask={saveTaskHandler} />
       <h2>Open Tasks </h2>
-      <ToDoList tasks={tasks} />
+      <ToDoList tasks={openTasks} handleTask={handleOpenTask} />
       <h2>Done Tasks</h2>
-      <ToDoList tasks={[]} />
+      <ToDoList tasks={doneTasks} handleTask={handleDoneTask} />
     </main>
   );
 }
