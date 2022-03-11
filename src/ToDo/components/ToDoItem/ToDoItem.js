a713a60d637330cbdbfad7091b5d912ba1b88c5b
Prim Asghar <mirpasghar@gmail.com>
2022-03-11 16:17:39 +0200

    Added delete icon linked with Click event

--- src/ToDo/components/ToDoItem/ToDoItem.js
+++ src/ToDo/components/ToDoItem/ToDoItem.js
@@ -1,10 +1,15 @@
 import React from "react";
+import img1 from "../../../Shared/Images/delete-24.png";
+
 import classes from "./ToDoItem.module.css";
 
-const ToDoItem = ({ text, handleTask }) => {
+const ToDoItem = ({ text, handleTask, handleRemoval }) => {
   return (
-    <li className={classes.item} onClick={handleTask}>
-      <p>{text}</p>
+    <li className={classes.item} onClick={handleTask} role="button">
+      <p>{text} </p>
+      <span>
+        <img src={img1} alt="trash icon for delete" onClick={handleRemoval} />
+      </span>
     </li>
   );
 };
