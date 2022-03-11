956f4f7e957b7c7b6350fa436dee4337e6e108f1
Prim Asghar <mirpasghar@gmail.com>
2022-03-11 16:12:45 +0200

    Created file for geneeral functions

---
+++ src/ToDo/utilityFunctions.js
@@ -0,0 +1,12 @@
+export const findTaskIndex = (tasksList, taskIndexToFind) =>
+  tasksList.findIndex((task) => task === taskIndexToFind);
+
+export const filteredTasks = (tasksList, taskToFilterIndex) =>
+  tasksList.filter((task, index) => index !== taskToFilterIndex);
+
+export const saveToLocalHost = (key, arr) =>
+  localStorage.setItem(key, JSON.stringify(arr));
+
+export const retrievefromLocalHost = (key) => {
+  JSON.parse(localStorage.getItem(key));
+};
